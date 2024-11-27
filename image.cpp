78ac548c6bfeb5aabf9a70065ff2527d1771696a
Manoj Subrahmanian <manoj.subramanian@gmail.com>
2024-11-27 15:02:46 +0530

    Reduce complexity

--- image.cpp
+++ image.cpp
@@ -1,8 +1,9 @@
 #include "./image.h"
 #include <vector>
-#include <cstdlib> // For rand() and srand()
+#include <cstdlib>  // For rand() and srand()
 #include <ctime>   // For time()
 #include <memory>
+#include <algorithm> // For std::transform
 
 Image::Image(int rows, int columns) : m_rows(rows), m_columns(columns) {
     // Seed the random number generator
@@ -12,22 +13,30 @@ Image::Image(int rows, int columns) : m_rows(rows), m_columns(columns) {
     pixels = std::make_unique<std::unique_ptr<int[]>[]>(rows);
     for (int i = 0; i < rows; ++i) {
         pixels[i] = std::make_unique<int[]>(columns);
-        for (int j = 0; j < columns; ++j) {
-            pixels[i][j] = std::rand() % 256;
-        }
+        InitializeRow(i);
     }
 }
 
+void Image::InitializeRow(int x) {
+    for (int y = 0; y < m_columns; ++y) {
+        pixels[x][y] = std::rand() % 256;
+    }
+}
+
+bool Image::IsValidIndex(int x, int y) const {
+    return x >= 0 && x < m_rows && y >= 0 && y < m_columns;
+}
+
 int Image::GetPixel(int x, int y) const {
-    if (x >= 0 && x < m_rows && y >= 0 && y < m_columns) {
+    if (IsValidIndex(x, y)) {
         return pixels[x][y];
     }
-    // Return a default value if out of bounds
+    //  Return a default value if out of bounds
     return 0;
 }
 
 void Image::SetPixel(int x, int y, int value) {
-    if (x >= 0 && x < m_rows && y >= 0 && y < m_columns) {
+    if (IsValidIndex(x, y)) {
         pixels[x][y] = value;
     }
 }
@@ -40,23 +49,29 @@ int Image::BrightenPixel(int x, int y) {
     int pixelValue = GetPixel(x, y);
     if (pixelValue > (255 - 25)) {
         SetPixel(x, y, 255);
-        return 1; // Pixel was attenuated
+        return 1;  // Pixel was attenuated
     } else {
         SetPixel(x, y, pixelValue + 25);
-        return 0; // Pixel was brightened
+        return 0;  // Pixel was brightened
     }
+}
+
+int Image::BrightenRow(int x) {
+    int attenuatedPixelCount = 0;
+    for (int y = 0; y < m_columns; y++) {
+        attenuatedPixelCount += BrightenPixel(x, y);
+    }
+    return attenuatedPixelCount;
 }
 
 int Image::Brighten() {
     if (!Validate()) {
-        return -1; // Indicate invalid image
+        return -1; //  Indicate invalid image
     }
 
     int attenuatedPixelCount = 0;
     for (int x = 0; x < m_rows; x++) {
-        for (int y = 0; y < m_columns; y++) {
-            attenuatedPixelCount += BrightenPixel(x, y);
-        }
+        attenuatedPixelCount += BrightenRow(x);
     }
     return attenuatedPixelCount;
 }
--- image.h
+++ image.h
@@ -13,6 +13,9 @@ class Image {
 
  private:
     bool Validate() const;
+    bool IsValidIndex(int x, int y) const;
     int BrightenPixel(int x, int y);
+    int BrightenRow(int x);
+    void InitializeRow(int x);
     std::unique_ptr<std::unique_ptr<int[]>[]> pixels;
 };
